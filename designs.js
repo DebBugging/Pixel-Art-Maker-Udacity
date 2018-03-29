df8fa3c7cd7d2e04e8580b0d5f009a8cc759dcaf
Deborah Castiglioni <33068355+DebBugging@users.noreply.github.com>
2018-03-29 17:01:40 -0400

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,46 @@
+let grid = $("#pixelCanvas");
+let color = $("#colorPicker");
+let drag = false;
+
+//Make grid when click submit
+$("#sizePicker").submit(function makeGrid(e) {
+    //reset grid
+    grid.html("");
+    //set the size
+    let rows = $("#inputHeight").val();
+    let cols = $("#inputWidth").val();
+    for (let i = 0; i < rows; i++) {
+        grid.append("<tr></tr>");
+        for (let x = 0; x < cols; x++) {
+            grid
+                .children()
+                .last()
+                .append("<td></td>");
+        }
+    }
+    e.preventDefault();
+});
+// Draw when the mouse is pressed down
+grid.on("mousedown", "td", function (e) {
+    e.preventDefault;
+    drag = true;
+    if (drag) {
+        $(e.target).css("background-color", color.val());
+    };
+});
+grid.on("mouseenter", "td", function (e) {
+    if (drag) {
+        $(e.target).css("background-color", color.val());
+    }
+});
+grid.on("mouseup", "td", function (e) {
+    drag = false;
+});
+grid.on('mouseleave', function (e) {
+    drag = false;
+});
+
+//Go back to white when double clicked.
+grid.dblclick("td", function (e) {
+    $(e.target).css("background-color", "white");
+});
